7574a3c51cf85222efebf6832969bc1536d8451f
galo salerno <galo@botmaker.com>
2022-07-14 17:18:32 -0300

    Add testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,29 @@
 import '@testing-library/jest-dom';
 import { presupuestoPrueba } from './constants/constants.js';
+import { getComprasByProyecto, postCompra } from './services/compras.js';
 import { createProyecto, getProyectoById } from './services/proyectos.js';
 
 const sumarCompras = (compras) => compras.reduce((a, b) => a + b, 0);
 
-test('Presupuesto fue creado', async () => {
-  const responseCreate = await createProyecto();
+test('Proyecto fue creado', async () => {
+  const proyecto = {
+    titulo: 'Proyecto Testing',
+    tipo: 'Testing',
+    organismo: 'Testing',
+    lineaFinanciamiento: 'Testing',
+    año: '2021/06/01',
+    unidadAcademica: 'Testing,',
+    areaTematica: 'Testing',
+    subsidio: 5777666,
+    fechaInicio: '2021/06/01',
+    fechaFin: '2022/06/01',
+    numeroExpediente: 1234,
+    numeroResolucion: 82171,
+    director: 'Testing',
+    codirector: 'Testing',
+    usuario: 'Testing',
+  };
+  const responseCreate = await createProyecto(proyecto);
   const id = responseCreate.id;
   const responseGet = await getProyectoById(id);
   expect(responseGet).toBeTruthy();
@@ -13,10 +31,11 @@ test('Presupuesto fue creado', async () => {
 
 test('Proyecto inexistente', async () => {
   const valorInexistente = 111123123213213;
-  await expect(getProyectoById(valorInexistente)).rejects.toContain("Bad request");
+  const response = await getProyectoById(valorInexistente);
+  expect(response.length).toEqual(0);
 });
 
-test('Monto por rubros equivale al total', async () => {
+test('Monto por rubros equivale al total', () => {
   const presupuesto = presupuestoPrueba; //Ideal cambiarlo por un fetch de un proyecto del back
   const total = presupuesto.total;
   const montos = (({
@@ -26,7 +45,7 @@ test('Monto por rubros equivale al total', async () => {
     viaticos,
     equipamiento,
     tecnico,
-    administracion
+    administracion,
   }) => ({
     insumos,
     bibliografia,
@@ -34,7 +53,7 @@ test('Monto por rubros equivale al total', async () => {
     viaticos,
     equipamiento,
     tecnico,
-    administracion
+    administracion,
   }))(presupuesto);
 
   const sumaDeMontos = Object.values(montos).reduce((a, b) => a + b);
@@ -42,32 +61,41 @@ test('Monto por rubros equivale al total', async () => {
 });
 
 test('Compra es realizada', async () => {
-  const idProyecto = 123; //Poner un Id correspondiente acá
+  const idProyecto = 42; //Poner un Id correspondiente acá
   const todasLasCompras = await getComprasByProyecto(idProyecto);
-  const compra = todasLasCompras.map(compra => Number(compra.monto));
+  const compra = todasLasCompras.map((compra) => Number(compra.monto));
   const totalComprasAntes = sumarCompras(compra);
-  const compraARealizar = { // Poner datos relevantes
-    fecha: fecha,
-    rubro: rubro,
-    subrubro: subrubro,
+  const montoCompra = 7357;
+  const compraARealizar = {
+    // Poner datos relevantes
+    fecha: '2019-01-31 21:00:00',
+    rubro: 'insumos',
+    subrubro: 'resmas',
     numeroCompra: 80,
-    proveedor: proveedor,
-    monto: monto,
+    proveedor: 'Garbarino S.A',
+    monto: montoCompra,
     estado: 'Comprado',
-    factura: 'factura-054',
-    nombre: nombre,
-    idProyecto: idProyecto
+    factura: 'factura-059',
+    nombre: 'Compra TEST',
+    idProyecto: idProyecto,
   };
-  const response = await postCompra(compraARealizar);
-  expect(totalCompras); // Fijate qué devolvemos al hacer una compra y hace un expect, basate en los anteriores, sino podés hacer otra suma de compras y ver que haya cambiado el monto.
+
+  await postCompra(compraARealizar);
+  const todasLasComprasNuevo = await getComprasByProyecto(idProyecto);
+  const compraNuevo = todasLasComprasNuevo.map((compra) =>
+    Number(compra.monto)
+  );
+  const totalComprasNuevo = sumarCompras(compraNuevo);
+
+  expect(totalComprasNuevo).toEqual(totalComprasAntes + montoCompra);
 });
 
 test('Compras de proyecto no son mayores que presupuesto del proyecto', async () => {
-  const idProyecto = 123; //Poner un Id correspondiente acá
+  const idProyecto = 42;
   const todasLasCompras = await getComprasByProyecto(idProyecto);
   const proyecto = await getProyectoById(idProyecto);
-  const compra = todasLasCompras.map(compra => Number(compra.monto));
+  const compra = todasLasCompras.map((compra) => Number(compra.monto));
   const totalCompras = sumarCompras(compra);
-  const presupuesto = proyecto.subsidio;
+  const presupuesto = proyecto[0].subsidio;
   expect(totalCompras).toBeLessThanOrEqual(presupuesto);
 });
