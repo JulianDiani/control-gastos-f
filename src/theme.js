529f0525a78856e341f88e95c5576f8d02b564f3
fernandobouchet <fernandobouchet@gmail.com>
2023-05-10 20:32:04 -0300

    Revert "Actualizado import para corregir el error : "Material-UI: the createMuiTheme function was renamed to createTheme.""
    This reverts commit 166dd977ac3561be45987618c4da9609aa1fa2f2.

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
-import { createTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core';
 
 // Acá podrían reemplazarse los colores y tipografías del tema, y eso se va a reflejar en toda la aplicación.
-const theme = createTheme({
+const theme = createMuiTheme({
   palette: {
     primary: {
       // Como ejemplo, cambiamos el primary por "verde UNaHur".
